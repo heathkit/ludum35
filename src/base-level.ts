e479aec9a0bb6372189827e9d8043e380ef518d2
Michael Giambalvo <heathkit@gmail.com>
2016-04-16 12:36:12 -0700

    Experiment with offsetting character bounding box.

--- src/base-level.ts
+++ src/base-level.ts
@@ -47,6 +47,8 @@ export class BaseLevel extends Phaser.State {
     //Add the sprite to the game and enable arcade physics on it
     this.sprite = this.game.add.sprite(10, 0, 'dude');
     this.game.physics.arcade.enable(this.sprite);
+    this.sprite.body.setSize(32,48,0,-18);
+    this.sprite.debug = true;
 
     //Change the world size to match the size of this layer
     this.groundLayer.resizeWorld();
