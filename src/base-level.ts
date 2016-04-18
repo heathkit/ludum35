6d086df85c16971ecc0aad3960102422e62189c6
Michael Giambalvo <heathkit@gmail.com>
2016-04-17 20:16:50 -0700

    Teleporting through vents works.

--- src/base-level.ts
+++ src/base-level.ts
@@ -140,7 +140,7 @@ export class Map {
         this.ventCallback(from, to);
         this.lastVentEventSent = this.game.time.totalElapsedSeconds();
       }
-      return true
+      return true;
     }
     return false;
   }
--- src/player.ts
+++ src/player.ts
@@ -15,7 +15,7 @@ export class Player {
     this.map = map;
 
     // Add the sprite to the game and enable arcade physics on it
-    this.sprite = game.add.sprite(10, 0, 'player');
+    this.sprite = game.add.sprite(10, 500, 'player');
     this.game.physics.arcade.enable(this.sprite);
     this.sprite.body.setSize(48, 48, 8, 6);
     this.sprite.debug = true;
@@ -27,8 +27,8 @@ export class Player {
     this.sprite.animations.add('steam', [ 0, 1, 2, 1 ], 7, true);
     this.sprite.animations.add('water', [ 3 ], 10, true);
 
-    this.waterState = new Water(this.sprite, this.map);
-    this.steamState = new Steam(this.sprite, this.map);
+    this.waterState = new Water(this.sprite, this.map, this.game);
+    this.steamState = new Steam(this.sprite, this.map, this.game);
 
     let dudeKey = this.game.input.keyboard.addKey(Phaser.Keyboard.ONE);
     let waterKey = this.game.input.keyboard.addKey(Phaser.Keyboard.TWO);
@@ -43,10 +43,13 @@ export class Player {
   }
 
   private changeState(newState: CharacterState) {
-    newState.init();
     if (this.currentState) {
-      this.currentState.cleanup();
+      if (!this.currentState.cleanup()) {
+        console.log("Not allowed to change state!");
+        return;
+      }
     }
+    newState.init();
     this.currentState = newState;
     console.log("Becoming state ", newState);
   }
@@ -70,17 +73,23 @@ export class Player {
 class CharacterState {
   sprite: Phaser.Sprite;
   map: Map
+  game: Phaser.Game;
 
-  constructor(sprite: Phaser.Sprite, map: Map) {
+  constructor(sprite: Phaser.Sprite, map: Map, game: Phaser.Game) {
     this.sprite = sprite;
     this.map = map;
+    this.game = game;
   }
 
   init() { console.log("Init unimplemented") }
 
   update(cursors: Phaser.CursorKeys) { console.log("Update unimplemented") }
 
-  cleanup() {}
+  // Clean up the state before switching. Will
+  // return false if the state does not allow switching.
+  cleanup(): boolean {
+      return true;
+  }
 }
 
 class Water extends CharacterState {
@@ -107,23 +116,73 @@ class Water extends CharacterState {
 }
 
 class Steam extends CharacterState {
-  init() {
-    this.sprite.animations.play("steam");
+  teleporting: boolean;
 
-    this.sprite.body.bounce.y = 0.4;
-    this.sprite.body.gravity.y = -2000;
+  lastExitVent: number;
+
+  init() {
+    this.lastExitVent = 0;
+    this.teleporting = false;
+    this.sprite.animations.play("steam");
+    this.startPhysics();
+
     this.map.ventCallback =
-        (from, to) => { console.log("Teleport between ", from, to); };
+        (from, to) => { this.teleportThroughPipe(from, to)};
   }
 
-  cleanup() { this.map.ventCallback = undefined; }
+  private startPhysics() {
+    this.sprite.body.bounce.y = 0.4;
+    this.sprite.body.gravity.y = -2000;
+  }
+
+  private disablePhysics() {
+    this.sprite.body.gravity.y=0;
+    this.sprite.body.velocity.y=0;
+    this.sprite.body.velocity.x=0;
+  }
+
+  private teleportThroughPipe(from, to) {
+    this.teleporting = true;
+    this.disablePhysics();
+    console.log("Teleport from ",from, to);
+
+    let shrink = this.game.add.tween(this.sprite.scale).to(
+      {x:0.1,y:0.1}, 500, Phaser.Easing.Cubic.Out);
+    let expand = this.game.add.tween(this.sprite.scale).to(
+      {x:1,y:1}, 500, Phaser.Easing.Cubic.Out);
+
+    let enterVent = this.game.add.tween(this.sprite).to(
+      from, 1000, Phaser.Easing.Cubic.Out);
+    let moveToExit = this.game.add.tween(this.sprite).to(
+      to, 1000, Phaser.Easing.Cubic.Out);
+
+    enterVent.chain(moveToExit);
+    enterVent.onStart.add(() => {shrink.start()});
+    moveToExit.chain(expand);
+
+    expand.onComplete.add(() => {
+      console.log("Teleport done");
+      this.teleporting = false;
+      this.startPhysics();
+      this.lastExitVent = this.game.time.elapsedMS;
+    })
+    enterVent.start();
+  }
+
+  cleanup() {
+    if (this.teleporting) {
+      return false;
+    }
+    this.map.ventCallback = undefined;
+    return true;
+  }
 
   update(cursors: Phaser.CursorKeys) {
     // Steam just rises uncontrollably.
 
-    // If we pass by a vent, get sucked into the ducts.
-    this.map.collideDucts(this.sprite);
-
-    // Maybe do a tween to shrink the cloud?
+    // Ignore collisions during teleport.
+    if (!this.teleporting) {
+      this.map.collideDucts(this.sprite);
+    }
   }
 }
